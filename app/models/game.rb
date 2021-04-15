80c138f5cc7c3cb8432a11106247f591331d6847
hajime <touken1996@yahoo.co.jp>
2021-04-15 23:34:32 +0900

    fix innner_text周りのエラー

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@
 
 class Game < ApplicationRecord
   scope :todays_match, -> { where(created_at: Time.zone.now.all_day).limit.distinct }
-  scope :recent, -> { order(id: :asc).limit(6).distinct }
+  scope :recent, -> { order(id: :desc).limit(6).distinct }
 end
