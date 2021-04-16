08a815909c9f8c545e36b6d2adfbd13bfb0f281b
hajime <touken1996@yahoo.co.jp>
2021-04-16 18:17:06 +0900

    add: 予想登録ページ

--- app/controllers/expects_controller.rb
+++ app/controllers/expects_controller.rb
@@ -1,9 +1,30 @@
 class ExpectsController < ApplicationController
+  
+  def index
+  end
+
   def new
     @expect = Expect.new
     @game = Game.find(params[:game_id])
   end
 
-  def index
-  end
+  def create
+    @expect = current_user.expects.new(expect_params)
+    if @expect.save
+   redirect_to game_expects_path
+  else
+    render :new
+    @expect = current_user.expects.new(expect_params)
+ end
+end
+
+
+
+def edit
+end
+
+private
+def expect_params
+  params.require(:expect).permit(:home_score,:away_score,).merge(game_id: params[:game_id])
+end
 end
