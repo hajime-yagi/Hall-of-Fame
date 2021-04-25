c707bc533f4958132dceaaff7ccb5ade5d733143
hajime <touken1996@yahoo.co.jp>
2021-04-26 00:41:26 +0900

    add:球団別ポイント表

--- app/controllers/team_ranks_controller.rb
+++ app/controllers/team_ranks_controller.rb
@@ -2,5 +2,15 @@ class TeamRanksController < ApplicationController
   def index
     @giants = User.giants.all.sum(:point)
     @tigers = User.tigers.all.sum(:point)
+    @dragons = User.dragons.all.sum(:point)
+    @baystars = User.baystars.all.sum(:point)
+    @carp = User.carp.all.sum(:point)
+    @swallows = User.swallows.all.sum(:point)
+    @hawks = User.hawks.all.sum(:point)
+    @marines = User.marines.all.sum(:point)
+    @lions = User.lions.all.sum(:point)
+    @orix = User.orix.all.sum(:point)
+    @fighters = User.fighters.all.sum(:point)
+    @eagles = User.eagles.all.sum(:point)
   end
 end
